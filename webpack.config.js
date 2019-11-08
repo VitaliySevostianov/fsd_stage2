a2e926c2481807ad74ce0b3b1cfca809536ba8e0
VitaliySevostianov <54743065+VitaliySevostianov@users.noreply.github.com>
2019-11-08 21:18:44 +0300

    fix of second build

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: __dirname + "/src/index.js", // webpack entry point. Module to start building dependency graph
     output: {
         path: __dirname + '/dist', // Folder to store generated bundle
-        filename: 'bundle.js',  // Name of generated bundle after build
-        publicPath: '/dist/index.html' // public URL of the output directory when referenced in a browser
+        filename: './bundle.js',  // Name of generated bundle after build
+        publicPath: '' // public URL of the output directory when referenced in a browser
     },
     module: {
         rules: [
