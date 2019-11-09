9ab20aee4dbabffd2af632ee09f69e00271d0477
VitaliySevostianov <54743065+VitaliySevostianov@users.noreply.github.com>
2019-11-09 15:09:17 +0300

    More components, but i think i will do refactoring of some of them

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import './index.scss';
 import dateInputMask from './Components/MaskedTF/MaskedTF'
-class TestClass {
-    constructor() {
-        let msg = "Using ES2015+ syntax";
-        console.log(msg);
-    }
-}
-let test = new TestClass();
 
 let input = document.querySelector('.masked-tf');
 
